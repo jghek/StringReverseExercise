4d84afbc30e6fc6780c2be39cbe3c64ae38ab543
ollie586 <153005025+ollie586@users.noreply.github.com>
2025-07-07 10:58:48 +0200

    implemented all test methodes

--- src/StringReverseExercise/StringReverser.cs
+++ src/StringReverseExercise/StringReverser.cs
@@ -1,4 +1,6 @@
-﻿namespace StringReverseExercise;
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace StringReverseExercise;
 
 public class StringReverser
 {
@@ -9,8 +11,11 @@ public class StringReverser
 	/// <returns>the modified string</returns>
 	public string Exercise1(string s)
 	{
-		return string.Empty;
-	}
+        var arr = s.Split(' ');
+        var b = arr.Reverse();
+        var result = string.Join(' ', b);
+        return result;
+    }
 
 	/// <summary>
 	/// Create a string from string A by reversing the characters in each word, keeping the word order intact.
@@ -19,8 +24,25 @@ public class StringReverser
 	/// <returns>the modified string</returns>
 	public string Exercise2(string s)
 	{
-		return string.Empty;
-	}
+        var arr = s.Split(' ');
+        var words = arr.Count() - 1;
+        var result = "";
+        foreach (var b in arr)
+        {
+            var letters = b.ToCharArray().Reverse();
+            foreach (var c in letters)
+            {
+                result += c;
+            }
+
+            if (arr[words] != b)
+            {
+                result += " ";
+            }
+        }
+
+        return result;
+    }
 
 	/// <summary>
 	/// Apply text transformations on step 2 and Reverse the casing: uppercase → lowercase, lowercase → uppercase
@@ -29,8 +51,36 @@ public class StringReverser
 	/// <returns>the modified string</returns>
 	public string Exercise31(string s)
 	{
-		return string.Empty;
-	}
+        var arr = s.Split(" ");
+        var words = arr.Count() - 1;
+        var result = "";
+        foreach (var b in arr)
+        {
+            var letters = b.ToCharArray().Reverse();
+            foreach (var c in letters)
+            {
+                if (char.IsUpper(c))
+                {
+                    result += char.ToLower(c);
+                }
+                else if (char.IsLower(c))
+                {
+                    result += char.ToUpper(c);
+                }
+                else
+                {
+                    result += c;
+                }
+
+            }
+
+            if (arr[words] != b)
+            {
+                result += " ";
+            }
+        }
+        return result;
+    }
 
 	/// <summary>
 	/// Apply text transformations on step 2 and Capitalize the first letter of each word
@@ -39,8 +89,40 @@ public class StringReverser
 	/// <returns>the modified string</returns>
 	public string Exercise32(string s)
 	{
-		return string.Empty;
-	}
+        var arr = s.Split(" ");
+        var words = arr.Count() - 1;
+        var result = "";
+        foreach (var b in arr)
+        {
+            var letters = b.ToCharArray().Reverse();
+            var count = 1;
+            foreach (var c in letters)
+            {
+                if (count == 1 && char.IsLower(c))
+                {
+                    result += char.ToUpper(c);
+                    count++;
+                }
+                else if (count != 1)
+                {
+                    result += char.ToLower(c);
+                    count++;
+                }
+                else
+                {
+                    result += c;
+                    count++;
+
+                }
+            }
+
+            if (arr[words] != b)
+            {
+                result += " ";
+            }
+        }
+        return result;
+    }
 
 	/// <summary>
 	/// Apply text transformations on step 2 and Capitalize the first letter of each sentence (Sentences end with ., ?, or !)
@@ -49,6 +131,44 @@ public class StringReverser
 	/// <returns>the modified string</returns>
 	public string Exercise33(string s)
 	{
-		return string.Empty;
-	}
+        var arr = s.Split(" ");
+        var result = "";
+        var words = arr.Count() - 1;
+        var count = 1;
+        foreach (var b in arr)
+        {
+            var letters = b.ToCharArray().Reverse();
+
+            foreach (var c in letters)
+            {
+                if (c == '.' || c == '!' || c == '?')
+                {
+                    count = 1;
+                    result += c;
+                }
+                else if (count == 1)
+                {
+                    result += char.ToUpper(c);
+                    count++;
+                }
+                else if (count != 1)
+                {
+                    result += char.ToLower(c);
+
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            if (arr[words] != b)
+            {
+                result += " ";
+            }
+        }
+        return result;
+    }
+
+
 }
