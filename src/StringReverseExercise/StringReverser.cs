fd888a503c24d8771d3cdd2412c01b8c9196602c
Jan Geert Hek :: Qtopia <j.g.hek@qtopia.nl>
2025-07-07 09:26:10 +0200

    Add documentation

--- src/StringReverseExercise/StringReverser.cs
+++ src/StringReverseExercise/StringReverser.cs
@@ -2,26 +2,51 @@
 
 public class StringReverser
 {
+	/// <summary>
+	/// From string A, create string B by reversing the order of words. Punctuation should stay attached to the word.
+	/// </summary>
+	/// <param name="s">the input</param>
+	/// <returns>the modified string</returns>
 	public string Exercise1(string s)
 	{
 		return string.Empty;
 	}
 
+	/// <summary>
+	/// Create a string from string A by reversing the characters in each word, keeping the word order intact.
+	/// </summary>
+	/// <param name="s">the input</param>
+	/// <returns>the modified string</returns>
 	public string Exercise2(string s)
 	{
 		return string.Empty;
 	}
 
+	/// <summary>
+	/// Apply text transformations on step 2 and Reverse the casing: uppercase → lowercase, lowercase → uppercase
+	/// </summary>
+	/// <param name="s">the input</param>
+	/// <returns>the modified string</returns>
 	public string Exercise31(string s)
 	{
 		return string.Empty;
 	}
 
+	/// <summary>
+	/// Apply text transformations on step 2 and Capitalize the first letter of each word
+	/// </summary>
+	/// <param name="s">the input</param>
+	/// <returns>the modified string</returns>
 	public string Exercise32(string s)
 	{
 		return string.Empty;
 	}
 
+	/// <summary>
+	/// Apply text transformations on step 2 and Capitalize the first letter of each sentence (Sentences end with ., ?, or !)
+	/// </summary>
+	/// <param name="s">the input</param>
+	/// <returns>the modified string</returns>
 	public string Exercise33(string s)
 	{
 		return string.Empty;
